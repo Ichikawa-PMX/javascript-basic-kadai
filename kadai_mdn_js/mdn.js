adfc762df821874b59184df32f0968daa7e9bd72
Ichikawa-PMX <gento-ichikawa@gr.paramix.co.jp>
2025-04-22 08:53:35 +0900

    Add files via upload

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,8 @@
+const DDD = new Date();
+console.log(DDD);
+
+const YYY =DDD.getFullYear();
+const MMM =DDD.getMonth()+1;
+const DD  =DDD.getDate();
+
+console.log(YYY + "年" + MMM + "月" + DD + "日");
