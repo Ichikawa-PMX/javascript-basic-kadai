388779c39cf86ea85771044aeadc265cc68390ad
Ichikawa-PMX <gento-ichikawa@gr.paramix.co.jp>
2025-04-22 11:03:55 +0900

    Delete kadai_021 directory

--- kadai_021/event.js
+++
@@ -1,9 +0,0 @@
-
-const aaa = document.getElementById('btn');
-const change = document.getElementById('text');
-
-setTimeout(() => {
-  aaa.addEventListener('click', () => {
-  change.textContent ='ボタンをクリックしました';
-  });
-},2000);
